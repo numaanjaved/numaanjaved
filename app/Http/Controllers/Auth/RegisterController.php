16eff8440070094681151cfd1ab24a641370c17b
numaanjaved <naumaan.javed@hotmail.com>
2022-09-25 19:24:08 +0500

    edited register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,7 +68,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-     'password' => Hash::make($data['password']),
+            'password' => Hash::make($data['password']),
         ]);
         User::last()->roles->attach($data['role']);
     }
