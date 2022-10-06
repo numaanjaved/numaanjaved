8da5606b420eef06de0d3e24667dca09e1ac458e
Numaan Javed <naumaan.javed@hotmail.com>
2022-10-06 18:04:52 +0500

    updated background of home page

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -12,7 +12,9 @@
 @section('body_data', $layoutHelper->makeBodyData())
 
 @section('body')
-    <div class="dark-mode relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div  style="background-image: url('images/background.gif') !important;
+    background-attachment: fixed;
+    background-size: 100% 100%;" class="dark-mode relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @yield('content')
     </div>
 @stop
